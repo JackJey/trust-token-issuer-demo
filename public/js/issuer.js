20082ffc45daeddc5f57b40d8d9440334c01fa36
Glitch (trust-token-issuer) <none>
2020-09-23 10:43:54 +0000

    🎷⛑ Updated with Glitch

--- public/js/issuer.js
+++ public/js/issuer.js
@@ -10,22 +10,8 @@ function base64decode(str) {
 document.on("DOMContentLoaded", async e => {
   console.log(e);
 
-  const { ISSUER, COMMITMENT } = await (await fetch(
-    "/.well-known/trust-token/key-commitment"
-  )).json();
-
-  // display chrome flag with key-commitment
-  const CMD = `/Applications/Google\\ Chrome\\ Canary.app/Contents/MacOS/Google\\ Chrome\\ Canary \\
-  --additional-trust-token-key-commitments='${JSON.stringify(COMMITMENT)}' \\
-  --auto-open-devtools-for-tabs \\
-  --v=1 \\
-  ${location.href} \\
-  1> /dev/null \\
-  2> /dev/null
-  `;
-
-  $("#flag").textContent = CMD;
-
+  const ISSUER = location.origin;
+  
   $("#yes").on("click", async () => {
     // issuer request
     await fetch(`/.well-known/trust-token/issuance`, {
@@ -39,36 +25,5 @@ document.on("DOMContentLoaded", async e => {
     // check token exists
     const token = await document.hasTrustToken(ISSUER);
     console.log(token);
-
-    // redemption request
-    await fetch(`/.well-known/trust-token/redemption`, {
-      method: "POST",
-      trustToken: {
-        type: "srr-token-redemption",
-        issuer: ISSUER,
-        refreshPolicy: "refresh"
-      }
-    });
-
-    // send SRR and echo Sec-Signed-Eedemption-Record
-    const res = await fetch(`/.well-known/trust-token/send-srr`, {
-      method: "POST",
-      trustToken: {
-        type: "send-srr",
-        issuer: ISSUER, // deprecated
-        issuers: [ISSUER]
-      }
-    });
-    const body = await res.text();
-    console.log(body);
-
-    // TODO: structured-header decode
-    const base64 = atob(body.match(/redemption-record=:(.*):/)[1])
-      .split(",")[0]
-      .match(/body=:(.*):/)[1];
-    const bytes = base64decode(base64);
-    const result = CBOR.decode(bytes.buffer);
-
-    $("#result").textContent = JSON.stringify(result, " ", " ");
   });
 });
